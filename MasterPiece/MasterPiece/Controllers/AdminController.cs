4ea6cf263037131ab76cd8e14b6d6cc85fc2d844
Ayahaldomi <ayahaldomi@gmail.com>
2024-08-31 12:31:37 +0300

    .,

---
+++ MasterPiece/MasterPiece/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MasterPiece.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult AdminDashboard()
+        {
+            return View();
+        }
+
+        public ActionResult AddPatient()
+        {
+            return View();
+        }
+    }
+}
